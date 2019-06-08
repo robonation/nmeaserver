3ba2e791e5026f98e044d9eafab0c897cdd4cb21
Felix Pageau <felix.pageau@gmail.com>
2019-06-08 01:23:35 -0700

    Switching to 0.1.4 since PiPy crashed halfway through publishing 0.1.3

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='nmea',
-    version='0.1.3',
+    version='0.1.4',
     description='Python framework for a NMEA 0183 TCP Server inspired by Flask developer API',
     url='https://github.com/robonation/nmea',
-    download_url = 'https://github.com/robonation/nmea/archive/v0.1.3.tar.gz',
+    download_url = 'https://github.com/robonation/nmea/archive/v0.1.4.tar.gz',
     author='Felix Pageau',
     author_email='pageau@robonation.org',
     license='Apache License 2.0',
